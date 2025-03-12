93f6f3c06e4d695e90b3c71f14e30f163a945a94
GiuseppeNapoletano <andreanap04@gmail.com>
2025-03-12 13:46:29 +0100

    feat: completed work

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ const commentoInput = document.getElementById("commento")
 const tipolavoroSelect = document.getElementById("tipolavoro")
 const codicepromoInput = document.getElementById("codicepromo")
 const risultato = document.getElementById("risultato")
+const promofailed = document.getElementById("promofailed")
 preventivoForm.addEventListener("submit", gestisciForm)
-const codiciPromo = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
-console.log(codiciPromo);
 
 
 
@@ -21,6 +20,7 @@ function gestisciForm(event) {
     const tipolavoro = tipolavoroSelect.value
     const codicepromo = codicepromoInput.value
     
+    
 
     let prezzoLavoro
     if (tipolavoro === "backend") {
@@ -31,21 +31,32 @@ function gestisciForm(event) {
         prezzoLavoro = (33.6 * 10)
     }
 
-    const sconto = codicisconto(codicepromo, prezzoLavoro)
-    console.log(sconto);
-    
-    
-    
+    const prezzoFinale = codicisconto(codicepromo, prezzoLavoro, promofailed)
+    const frasePrezzoFinale = `Prezzo finale\n ${prezzoFinale}€`
+    risultato.innerText = frasePrezzoFinale
+     
+   
     preventivoForm.reset()
 
 }
 
-function codicisconto(codiceFunction, prezzoFunction) {
-    let sconto25
-    if (codiceFunction === "YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24") {
-        sconto25 = (prezzoFunction * 0.25)
+function codicisconto(coupon, totalelavoro, couponsbagliato) {
+    const arrayPromo = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+    const ammontare = totalelavoro
+    let prezzoScontato
+    const messaggio = `Codice promo errato! Inseriscine uno nuovo!`
+    const codiceCoupon  = coupon.toUpperCase()
+    if (arrayPromo.includes(codiceCoupon)) {
+        prezzoScontato = ammontare * 0.75
+        couponsbagliato.innerText = ``
+    } else if (codiceCoupon === ``) {
+        prezzoScontato = ammontare 
+        couponsbagliato.innerText = ``
     } else {
-        sconto25 = 0
+        prezzoScontato = ammontare
+        couponsbagliato.innerText = messaggio
     }
-    return sconto25
+
+    return prezzoScontato
 }
+
